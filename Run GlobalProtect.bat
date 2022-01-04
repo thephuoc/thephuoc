10a9b524491b3960a2857f3f41059c5312c7980a
thephuoc <90171141+thephuoc@users.noreply.github.com>
2022-01-04 08:51:45 +0700

    Add files via upload

---
+++ Run GlobalProtect.bat
@@ -0,0 +1,6 @@
+echo off
+cd "C:\Program Files\Palo Alto Networks\GlobalProtect"
+start PanGPA.exe
+sc start PanGPS
+rem sc config PanGPS start= demand
+rem pause
---
+++ Stop GlobalProtect.bat
@@ -0,0 +1,5 @@
+echo off
+taskkill /im PanGPA.exe
+sc stop PanGPS
+rem sc config PanGPS start= demand
+rem pause
