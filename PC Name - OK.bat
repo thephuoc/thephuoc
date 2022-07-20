f45309d33e85b52691289b6ad5e9d9eb18cb0232
thephuoc <90171141+thephuoc@users.noreply.github.com>
2022-07-20 08:45:53 +0700

    Update PC Name - OK.bat

--- PC Name - OK.bat
+++ PC Name - OK.bat
@@ -1,4 +1,6 @@
 @echo off
+@echo off
+@echo off
 SET /P PCNAME=Please enter your name: 
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d %PCNAME% /f
 WMIC computersystem where caption="%computername%" rename "%PCNAME%"
